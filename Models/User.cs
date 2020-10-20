ffb18eb951c07bab519d35c0f26dfa245b4ac003
Yaeli <yaeli4082@gmail.com>
2020-10-20 15:04:38 +0300

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication10.Models
+{
+    public class User
+    {
+        string FirstName;
+        string LastName;
+        string Id;
+        string Phone;
+        string Mail;
+        float hight;
+        float width;
+        float age;
+
+
+    }
+}
